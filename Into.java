40732a62a285e42ae3adba495664a39929a579d4
psychic-coder <rohitganguly450@gmail.com>
2024-01-03 15:52:27 +0530

    oy

--- Into.java
+++ Into.java
@@ -3,10 +3,6 @@ package Localrepo;
 public class Into {
     public static void main(String[] args) {
         System.out.println("You");
-<<<<<<< HEAD
         System.out.println("hey");
-=======
-        System.out.println("brocode");
->>>>>>> main
     }
 }
